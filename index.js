1a3092d563b248bdf7296f758c90cf0028941698
Frank Metzulat <frank.metzulat@gmail.com>
2022-03-25 11:44:51 +0100

    Add sumUp service

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const express = require('express');
 const app = express()
+const axios = require('axios');
 const PORT = process.env.PORT || 5000;
+const qs = require('qs');
+const lib = require('./src/lib')
+const server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.get('/', (req, res) => res.send('Hello world!'))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.json())
+
+app.post('/sumup', (req, res) => {
+  const json = req.body
+  const  result = lib.checksum(lib.sumArray(json.address.values))
+  res.json({result: result});
+})
+
+app.get('/', (req, res) => {
+  try {
+    const response =  axios({
+      url: 'http://localhost:5000/sumup',
+      method: 'post',
+      data: {
+        address: {
+         values:[
+           74,
+           117,
+           115,
+           116,
+           79,
+           110]
+        }
+      },
+      paramsSerializer: function(params) {
+        return qs.stringify(params, {arrayFormat: 'repeat'})
+      }
+    }).then(function (response) {
+      res.json(response.data)
+    })    
+  } catch (error) {
+      console.log(error)
+  } 
+})
